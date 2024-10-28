cb46434672eb3ae79f0eedcd726ecc1afe87245b
Mel <eaaalov@students.eaaa.dk>
2024-10-28 11:23:29 +0100

    hihi

--- QSParsingConsoleApp/obj/Debug/net8.0/QSParsingConsoleApp.AssemblyInfo.cs
+++ QSParsingConsoleApp/obj/Debug/net8.0/QSParsingConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QSParsingConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c021a7af1c81a2806c16d06edc3c1645ab6d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("QSParsingConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QSParsingConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
